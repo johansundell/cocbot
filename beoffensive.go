48eceb9e561be2aed7616dd936a5c7d890a58d34
Johan Sundell <johan@pixpro.net>
2017-04-27 14:10:47 +0200

    Added rude command

---
+++ beoffensive.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func init() {
+	key := commandFunc{"!be offensive", "", ""}
+	lockMap.Lock()
+	defer lockMap.Unlock()
+	botFuncs[key] = func(command string) (string, error) {
+		if strings.HasPrefix(command, key.command) {
+			out, err := exec.Command("fortune", "-o").Output()
+			if err != nil {
+				return "", err
+			}
+			return string(out), nil
+		}
+		return "", nil
+	}
+}
