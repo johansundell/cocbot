73d034aa514de26f0ab33202903f05d32e40f107
Johan Sundell <johan@pixpro.net>
2017-05-04 10:22:02 +0200

    Cleanup

--- hi.go
+++ hi.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-
-	"github.com/bwmarrin/discordgo"
 )
 
 func init() {
@@ -12,11 +10,6 @@ func init() {
 	defer lockMap.Unlock()
 	botFuncs[key] = func(command string, ctx context.Context) (string, error) {
 		if command == "!test" {
-			if s := ctx.Value("sess"); s != nil {
-				if m := ctx.Value("msg"); m != nil {
-					s.(*discordgo.Session).ChannelMessageSend(m.(*discordgo.MessageCreate).ChannelID, "testing context")
-				}
-			}
 			return "Hi master", nil
 		}
 		return "", nil
