73a2c80f78002c6ea7f90b1dee44f3b1d7f7ae52
Johan Sundell <johan@pixpro.net>
2017-09-21 13:37:34 +0200

    Fixed duplicate to only report active clan member

--- reporterduplicate.go
+++ reporterduplicate.go
@@ -10,27 +10,13 @@ import (
 
 func reporterDuplicate(s *discordgo.Session) {
 	ticker := time.NewTicker(15 * time.Minute)
+	//ticker := time.NewTicker(1 * time.Minute)
 	quit := make(chan struct{})
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				rows, err := db.Query("SELECT GROUP_CONCAT(name) as usernames, tag, count(*) AS c FROM members GROUP BY tag HAVING c > 1")
-
-				if err != nil {
-					log.Println(err)
-				} else {
-					msg := ""
-					for rows.Next() {
-						name, tag, c := "", "", 0
-						rows.Scan(&name, &tag, &c)
-						msg += fmt.Sprintf("Found duplicate member in clan %s, with tag %s has %d accounts", name, tag, c)
-					}
-					rows.Close()
-					if len(msg) > 0 {
-						sendMessage(s, msg, true)
-					}
-				}
+				findDuplicate(s)
 
 			case <-quit:
 				ticker.Stop()
@@ -38,4 +24,31 @@ func reporterDuplicate(s *discordgo.Session) {
 			}
 		}
 	}()
+}
+
+func findDuplicate(s *discordgo.Session) {
+	rows, err := db.Query("SELECT GROUP_CONCAT(name) as usernames, tag, count(*) AS c FROM members GROUP BY tag HAVING c > 1")
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		msg := ""
+		for rows.Next() {
+			name, tag, c := "", "", 0
+			rows.Scan(&name, &tag, &c)
+			if clan, err := cocClient.GetClanInfo(myClanTag); err == nil {
+				for _, m := range clan.MemberList {
+					if m.Tag == tag {
+						msg += fmt.Sprintf("Found duplicate member in clan %s, with tag %s has %d accounts", name, tag, c)
+					}
+				}
+			}
+		}
+		rows.Close()
+		if len(msg) > 0 {
+			sendMessage(s, msg, true)
+			//log.Println(msg)
+		}
+		//log.Println("test", msg)
+	}
 }
